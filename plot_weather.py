6f02b36baace43e76d6520d21314b7abf43e8817
Francisco A. Iglesias <franciscoaiglesias@gmail.com>
2022-10-12 15:47:16 -0300

    juto las listas de master y wea

--- plot_weather.py
+++ plot_weather.py
@@ -1,5 +1,6 @@
 from calendar import month
 from pickle import TRUE
+from re import M
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,10 +21,12 @@ COL_NAMES_WEA = ["DATE", "HOUR(UT)", "TEMP", "PRESS", "HUM", "WSP", "WDIR"]
 NBINS = 20
 REMOVE = {"Sky_T": [-990], "Amb_T": [], "WindS": [], "Hum%": [], "DewP": [],
           "C": [], "W": [], "R": [], "Cloud_T": [], "DATE_DIF": [], "WV": []}
-variables = ["TEMP", "PRESS", "HUM", "WSP", "WDIR", "WV", "date_diff"]  # variables to plot
+variables_w = ["TEMP", "PRESS", "HUM", "WSP", "WDIR", "WV", "DATE_DIF"]  # variables to plot
 units = ["  [$^\circ C$]", "  [mm Hg]","  [%]","  [m$s^{-1}$]","  [Deg]","  [mm]",""]
-remove_weather_min = {"TEMP":[-20], "PRESS": [], "HUM": [0], "WSP": [0], "WDIR": [], "WV": [], "date_diff": [0]}
-remove_weather_max = {"TEMP":[40], "PRESS": [], "HUM": [150], "WSP": [40], "WDIR": [], "WV": [], "date_diff": [3600]}
+remove_weather_min = {"TEMP":[-20], "PRESS": [], "HUM": [0], "WSP": [0], "WDIR": [], "WV": [], "DATE_DIF": [0]}
+remove_weather_max = {"TEMP":[40], "PRESS": [], "HUM": [150], "WSP": [40], "WDIR": [], "WV": [], "DATE_DIF": [3600]}
+variables=['HUM', 'TEMP','Sky_T', 'WSP', 'Cloud_T', 'WV', 'PRESS', 'WDIR']
+
 
 # create opath
 os.makedirs(OPATH, exist_ok=True)
@@ -89,7 +92,7 @@ for g in mf_wea:
     df_wea["DATE"] = pd.to_datetime(df_wea["DATE"], format="%Y%m%d")
     df_wea["HOUR(UT)"] = pd.to_timedelta(df_wea["HOUR(UT)"])
     df_wea["DATE_TIME"] = df_wea["DATE"]+df_wea["HOUR(UT)"]
-    df_wea['date_diff'] = df_wea['DATE_TIME'].diff().dt.total_seconds()
+    df_wea['DATE_DIF'] = df_wea['DATE_TIME'].diff().dt.total_seconds()
     df_all_wea.append(df_wea)
 df_all_wea = pd.concat(df_all_wea, ignore_index=True)
 df_all_wea["WV"] = water_vapor(df_all["Amb_T"], df_all["Hum%"])
@@ -98,28 +101,48 @@ df_all_wea["WV"] = water_vapor(df_all["Amb_T"], df_all["Hum%"])
 df_weather = df_all_wea.loc[(df_all_wea["DATE_TIME"].dt.hour > 9) & (df_all_wea["DATE_TIME"].dt.hour < 22)]
 
 # remove values
-for var in variables:
+for var in variables_w:
     for i in remove_weather_min[var]:
         df_2 = df_weather[df_weather[var] <= i].index
         df_weather = df_weather.drop(df_2)
 
-for var in variables:
+for var in variables_w:
     for i in remove_weather_max[var]:
         df_3 = df_weather[df_weather[var] >= i].index
         df_weather= df_weather.drop(df_3)
 
-months = df_weather['DATE_TIME'].dt.month.unique()
-years = np.sort(df_weather['DATE_TIME'].dt.year.unique())
+#Renaming and creating columns
+df_final["ORIGIN"] = 'M'
+df_weather["ORIGIN"] = "W"
+df_final.rename(columns = {'DATE':'DATE_TIME'}, inplace = True)
+df_final.rename(columns = {'DAY':'DATE'}, inplace = True)
+df_final.rename(columns = {'Amb_T':'TEMP'}, inplace = True)
+df_final.rename(columns = {'Hum%':'HUM'}, inplace = True)
+df_final.rename(columns = {'WindS':'WSP'}, inplace = True)
+
+
+#Creating combined dataframe
+df_combined = pd.DataFrame
+df_combined = pd.concat([df_final,df_weather ],ignore_index = True,sort = False)
+df_combined.drop(['C'], axis=1, inplace = True)
+df_combined.drop(['W'], axis=1, inplace = True)
+df_combined.drop(['R'], axis=1, inplace = True)
+df_combined.drop(['DewP'], axis=1, inplace = True)
+
+
+months = df_combined['DATE_TIME'].dt.month.unique()
+years = np.sort(df_combined['DATE_TIME'].dt.year.unique())
 
 j = 0
 # Ploting graphics
 for i in variables:
+    print(i)
     fig1, (ax1, ax2, ax3) = plt.subplots(3)
     fig1.set_size_inches(13, 4)
     median = []
     colors = '#e4e8f0'
     for m in months:
-        tot = df_weather.loc[df_weather['DATE_TIME'].dt.month == m, i]
+        tot = df_combined.loc[(df_combined['DATE_TIME'].dt.month == m) &(df_combined[i]!= np.nan), i]
         median.append(tot)
     ax1 = plt.subplot(1, 3, 1)
     bp = ax1.boxplot(median, showfliers=False, patch_artist = True)
@@ -139,7 +162,7 @@ for i in variables:
     # Ploting Year median and standar deviation
     median_y = []
     for y in years:
-        total = df_weather.loc[df_weather['DATE_TIME'].dt.year == y, i]
+        total = df_combined.loc[(df_combined['DATE_TIME'].dt.year == y) &(df_combined[i]!= np.NAN), i]
         median_y.append(total)
     ax2 = plt.subplot(1, 3, 2)
     nyear = np.arange(len(years))
@@ -149,7 +172,7 @@ for i in variables:
     for patch in bp2['boxes']:
         patch.set_facecolor(color = 'white')  
     ax2.set_ylabel(i+units[j])
-    ax2.set_xlabel('Year from 2000')
+    ax2.set_xlabel('Year from 00')
     ax2.set_xticks(nyear[::2])
     ax2.minorticks_on()
     ax2.yaxis.grid(which="minor", linestyle=':', linewidth=0.7)
@@ -158,12 +181,13 @@ for i in variables:
 
     # Ploting cumulative histogram
 
-    val_medio=round(df_weather[i].mean(),3)
-    mediana=df_weather[i].median()
-    max= df_weather[i].max()
-    min=df_weather[i].min()
+    val_medio=round(df_combined[i].mean(),1)
+    mediana=df_combined[i].median()
+    max= df_combined[i].max()
+    min=df_combined[i].min()
     ax3 = plt.subplot(1, 3, 3)
-    ax3.hist(df_weather[i], weights=df_weather["date_diff"], density=True,
+
+    ax3.hist(np.isnan(df_combined[i]), weights=df_combined["DATE_DIF"], density=True,
              cumulative=True, color="grey", bins=NBINS, log=True)
     ax3.set_xlabel(i+units[j])
     ax3.minorticks_on()
@@ -171,10 +195,10 @@ for i in variables:
     ax3.yaxis.grid(True, which='Major')
     ax3.xaxis.grid(which="minor", linestyle=':', linewidth=0.7)
     ax3.xaxis.grid(True, which='Major')
-    ax3.text(5,0.01,"min: "+str(min))
-    ax3.text(5,0.006,"max: "+str(max))
-    ax3.text(5,0.004,"medium value:  "+str(val_medio))
-    ax3.text(5,0.0025,"median: "+str(mediana))
+    ax3.text("Min: "+str(min),horizontalalignment = 'center',verticalalignment='bottom',fontsize = 11)
+    # ax3.text("Max: "+str(max),fontsize = 11)
+    # ax3.text("Mean value:  "+str(val_medio),fontsize = 11)
+    # ax3.text("Median: "+str(mediana),fontsize = 11)
     plt.tight_layout()
     plt.savefig(OPATH+'/'+i, dpi=300)
     plt.close()
